5e1b45ce8f8203820f291131ed8908e94cad8613
Anujit Deb <anujitdeb99@gmail.com>
2023-01-15 16:57:34 +0600

    question request view for admin frontend done

--- resources/views/backend/pages/users/questionRequest.blade.php
+++ resources/views/backend/pages/users/questionRequest.blade.php
@@ -9,122 +9,89 @@
 
 
 
-    <div class="flex flex-col mt-8">
-        <div class="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-            <div class="inline-block min-w-full overflow-auto align-middle border-b border-gray-200 shadow sm:rounded-lg">
-                <table class="min-w-full text-center">
-                    <thead>
-                        <tr>
-                            <th
-                                class="text-center px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                                Question</th>
-                            <th
-                                class="text-center px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                                Solution</th>
-                            <th
-                                class="text-center px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                                Institution Name</th>
-                            <th
-                                class="text-center px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                                Exam Name</th>
-                            <th
-                                class="text-center px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                                Course Name</th>
-                            <th
-                                class="text-center px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                                Course Title</th>
-                            <th
-                                class="text-center px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                                Approve</th>
-                            <th
-                                class="text-center px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                                Disapprove</th>
-                        </tr>
-                    </thead>
 
-                    <tbody class="bg-white text-center">
+    <!-- BEGIN: Datatable -->
+    <div class="intro-y datatable-wrapper box p-5 mt-5">
+        <table class="table table-report table-report--bordered display datatable w-full">
+            <thead>
+            <tr>
+                <th class="border-b-2 whitespace-no-wrap">Question</th>
+                <th class="border-b-2 whitespace-no-wrap">Solution</th>
+                <th class="border-b-2 text-center whitespace-no-wrap">Institution Name</th>
+                <th class="border-b-2 text-center whitespace-no-wrap">Exam Name</th>
+                <th class="border-b-2 text-center whitespace-no-wrap">Course Name</th>
+                <th class="border-b-2 text-center whitespace-no-wrap">Course Title</th>
+{{--                <th class="border-b-2 text-center whitespace-no-wrap">Status</th>--}}
+                <th class="border-b-2 text-center whitespace-no-wrap">Actions</th>
+            </tr>
+            </thead>
+            <tbody>
 
-                        @foreach($questions as $question)
-                            <tr>
+            @foreach($questions as $question)
+                <tr>
+                    @php
+                        $pdf = strpos($question->question_name, ".pdf");
+                        $doc = strpos($question->question_name, ".doc");
+                    @endphp
 
-                                @php
-                                    $pdf = strpos($question->question_name, ".pdf");
-                                    $doc = strpos($question->question_name, ".doc");
-                                @endphp
+                    <td class="text-center border-b">
+                        <a href="{{route('question-view', $question->id)}}">
+                            <div class="flex items-center">
+                                <div class="flex-shrink-0 w-10 h-10">
+                                    @if(!$pdf && !$doc)
+                                        <img class="w-10 h-10 rounded-full" src="{{asset('/questions/' . $question->question_name)}}"
+                                             alt="admin dashboard ui">
+                                    @else
+                                        <img class="w-10 h-10 rounded-full" src="/test/question_thumbnail.jpg"
+                                             alt="admin dashboard ui">
+                                    @endif
+                                </div>
+                            </div>
+                        </a>
+                    </td>
 
-                                <td class="text-center px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <a href="{{route('question-view', $question->id)}}">
-                                        <div class="flex items-center">
-                                            <div class="flex-shrink-0 w-10 h-10">
-                                                @if(!$pdf && !$doc)
-                                                    <img class="w-10 h-10 rounded-full" src="{{asset('/questions/' . $question->question_name)}}"
-                                                         alt="admin dashboard ui">
-                                                @else
-                                                    <img class="w-10 h-10 rounded-full" src="/test/question_thumbnail.jpg"
-                                                         alt="admin dashboard ui">
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </a>
-                                </td>
+                    @php
+                        $solPdf = strpos($question->solution_name, ".pdf");
+                        $solDoc = strpos($question->solution_name, ".doc");
+                    @endphp
 
-                                @php
-                                    $solPdf = strpos($question->solution_name, ".pdf");
-                                    $solDoc = strpos($question->solution_name, ".doc");
-                                @endphp
-
-                                <td class="text-center px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <a href="{{route('solution-view', $question->id)}}">
-                                        <div class="flex items-center">
-                                            <div class="flex-shrink-0 w-10 h-10">
-                                                @if(!$solPdf && !$solDoc)
-                                                    <img class="w-10 h-10 rounded-full" src="{{asset('/solutions/' . $question->solution_name)}}"
-                                                         alt="admin dashboard ui">
-                                                @else
-                                                    <img class="w-10 h-10 rounded-full" src="/test/question_thumbnail.jpg"
-                                                         alt="admin dashboard ui">
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </a>
-                                </td>
-
-                                <td class="text-center px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                <span
-                                    class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">{{$question->institution}}</span>
-                                </td>
-                                <td class="text-center px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                <span
-                                    class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">{{$question->exam_name}}</span>
-                                </td>
-
-                                <td class="text-center px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                <span
-                                    class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">{{$question->course_title}}</span>
-                                </td>
-                                <td class="text-center px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                <span
-                                    class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">{{$question->course_code}}</span>
-                                </td>
-
-                                <td
-                                    class="text-center text-blue-600 px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="pl-5 side-menu__icon"><i data-feather="smile"></i></div>
-                                </td>
-                                <td
-                                    class="text- text-red-600 px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="pl-5 side-menu__icon"><i data-feather="frown"></i></div>
-                                </td>
-                            </tr>
-                        @endforeach
-
-                    </tbody>
-                </table>
-            </div>
-        </div>
+                    <td class="text-center border-b">
+                        <a href="{{route('solution-view', $question->id)}}">
+                            <div class="flex items-center">
+                                <div class="flex-shrink-0 w-10 h-10">
+                                    @if(!$solPdf && !$solDoc)
+                                        <img class="w-10 h-10 rounded-full" src="{{asset('/solutions/' . $question->solution_name)}}"
+                                             alt="admin dashboard ui">
+                                    @else
+                                        <img class="w-10 h-10 rounded-full" src="/test/question_thumbnail.jpg"
+                                             alt="admin dashboard ui">
+                                    @endif
+                                </div>
+                            </div>
+                        </a>
+                    </td>
+                    <td class="text-center border-b">{{$question->institution}}</td>
+                    <td class="text-center border-b">{{$question->exam_name}}</td>
+                    <td class="text-center border-b">{{$question->course_title}}</td>
+                    <td class="text-center border-b">{{$question->course_code}}</td>
+                    {{--<td class="w-40 border-b">
+                        <div class="flex items-center sm:justify-center text-theme-9"> <i data-feather="check-square" class="w-4 h-4 mr-2"></i> Active </div>
+                    </td>--}}
+                    <td class="border-b w-5">
+                        <div class="flex sm:justify-center items-center">
+                            <a class="flex items-center mr-3" href=""> <i data-feather="check-square" class="w-4 h-4 mr-1"></i> Approve </a>
+                            <a class="flex items-center text-theme-6" href=""> <i data-feather="trash-2" class="w-4 h-4 mr-1"></i> Disapprove </a>
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
     </div>
+    <!-- END: Datatable -->
 
 
 
 @endsection
+
 
